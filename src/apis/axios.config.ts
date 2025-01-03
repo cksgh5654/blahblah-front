dd2d1a76592b5c092d6ad0fa7078018e36001d83
DonggunLim <Ldonggun6766@gmail.com>
2025-01-04 01:59:37 +0900

    feat: axios baseInstance 생성

---
+++ src/apis/axios.config.ts
@@ -0,0 +1,23 @@
+import axios, { HttpStatusCode, isAxiosError } from "axios";
+
+export const baseInstance = axios.create({
+  baseURL: "/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+baseInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (isAxiosError(error)) {
+      if (error.status === HttpStatusCode.BadRequest) {
+        console.error(error);
+      }
+      if (error.status === HttpStatusCode.Unauthorized) {
+        console.error(error);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
