7708c4d8d42019c6c099e8875adfe9f00b7281a3
DonggunLim <Ldonggun6766@gmail.com>
2025-01-05 12:18:56 +0900

    feat: email 로그인 기능 추가

--- src/pages/SigninPage.tsx
+++ src/pages/SigninPage.tsx
@@ -1,9 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import { signinWithEmail } from "../apis/auth.api";
+import EmailAuthForm, { EmailFormDataType } from "../components/EmailAuthForm";
+
 const SigninPage = () => {
+  const navigate = useNavigate();
+  const handleEmailSignin = (formData: EmailFormDataType) => {
+    signinWithEmail(formData) //
+      .then(() => navigate("/"));
+  };
   const handleGoogleOauth = async () => {
     window.location.href = "api/auth/google-entry-url";
   };
   return (
     <>
+      <EmailAuthForm type="signin" onSubmit={handleEmailSignin} />
       <button onClick={handleGoogleOauth}>구글 계정으로 로그인</button>
     </>
   );
