1fb7f8cb38352a9c26cc687b6edb4e83df8cf312
DonggunLim <Ldonggun6766@gmail.com>
2025-01-05 11:02:27 +0900

    feat: reqeustSignupOtp,sendOtp api(otp 이메일 요청, opt 인증 요청) 추가

---
+++ src/apis/auth.api.ts
@@ -0,0 +1,32 @@
+import { EmailFormDataType } from "../components/EmailAuthForm";
+import { baseInstance } from "./axios.config";
+
+export const requestSignupOtp = async (data: EmailFormDataType) => {
+  try {
+    const response = await baseInstance.post("/auth/signup/otp", data);
+    if (response.data.isError) {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw error;
+    }
+  }
+};
+
+export const sendOtp = async (otp: string) => {
+  try {
+    const response = await baseInstance.post("/auth/signup/otp/verify", {
+      otp,
+    });
+    if (response.data.isError) {
+      throw new Error(response.data.message);
+    }
+    return response.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw error;
+    }
+  }
+};
