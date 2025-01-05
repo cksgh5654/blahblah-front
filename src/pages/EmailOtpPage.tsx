fac9f62a27b7b0f0d554e365acb0c66e8224266e
DonggunLim <Ldonggun6766@gmail.com>
2025-01-06 00:52:54 +0900

    design: EmailOtpPgae ui 작업

--- src/pages/EmailOtpPage.tsx
+++ src/pages/EmailOtpPage.tsx
@@ -1,6 +1,9 @@
 import { FormEvent, useState } from "react";
 import { sendOtp } from "../apis/auth.api";
 import { useNavigate } from "react-router-dom";
+import MainLogo from "../components/Icons/MainLogo";
+import BaseInput from "../components/Input/BaseInput";
+import BaseButton from "../components/Button/BaseButton";
 
 const EmailOtpPage = () => {
   const navigate = useNavigate();
@@ -13,10 +16,20 @@ const EmailOtpPage = () => {
       .then(() => navigate("/"));
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" onChange={(e) => setOtp(e.target.value)} />
-      <button>이메일 인증 하기</button>
-    </form>
+    <div
+      className="w-screen flex justify-center items-center"
+      style={{ height: "calc(-52px + 100vh)" }}
+    >
+      <form onSubmit={handleSubmit} className="w-96 flex flex-col gap-y-4">
+        <MainLogo />
+        <BaseInput
+          withLabel="OTP 번호"
+          onChange={(e) => setOtp(e.target.value)}
+          maxLength={6}
+        />
+        <BaseButton>이메일 인증 하기</BaseButton>
+      </form>
+    </div>
   );
 };
 
