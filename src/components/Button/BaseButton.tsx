4e0fd390a25b5f5ae13e708938f2f60708a5799f
DonggunLim <Ldonggun6766@gmail.com>
2025-01-08 11:04:05 +0900

    fix: BaseButton props 추가

--- src/components/Button/BaseButton.tsx
+++ src/components/Button/BaseButton.tsx
@@ -1,14 +1,18 @@
 import { ButtonHTMLAttributes } from "react";
 
 interface BaseButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
-const baseStyle =
-  "w-full bg-violet-800 text-white font-bold px-6 py-2 rounded-lg";
+const baseStyle = "bg-violet-800 text-white font-bold px-6 py-2 rounded-lg";
 
-const BaseButton = ({ onClick, children, className }: BaseButtonProps) => {
+const BaseButton = ({
+  onClick,
+  children,
+  className,
+  ...rest
+}: BaseButtonProps) => {
   const btnStyle = className ? `${baseStyle} ${className}` : baseStyle;
   return (
     <>
-      <button onClick={onClick} className={btnStyle}>
+      <button onClick={onClick} className={btnStyle} {...rest}>
         {children}
       </button>
     </>
