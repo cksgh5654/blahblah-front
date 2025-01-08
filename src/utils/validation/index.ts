eb52533e1af04e1a5815a19a63cf46a22304080d
DonggunLim <Ldonggun6766@gmail.com>
2025-01-08 18:29:57 +0900

    fix: password valdiation 특수문자도 추가

--- src/utils/validation/index.ts
+++ src/utils/validation/index.ts
@@ -14,13 +14,14 @@ export const PasswordValidation = (password: string) => {
   if (!password.length) {
     return { result: false, message: "패스워드를 입력해주세요." };
   }
-  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  const passwordRegex =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   const result = passwordRegex.test(password);
   return {
     result,
     message: result
       ? null
-      : "패스워드는 8자이상, 영문 대소문자와 숫자를 포함해야 합니다.",
+      : "8자이상, 영문 대소문자와 숫자, 특수문자를 포함해야 합니다.",
   };
 };
 
