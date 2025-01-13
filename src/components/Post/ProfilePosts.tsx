8ecc43544abdfee13e0fd8f8b15c6c05915aa07a
DonggunLim <Ldonggun6766@gmail.com>
2025-01-14 02:18:43 +0900

    design: profile 페이지 데이터 없을때 뷰 추가

--- src/components/Post/ProfilePosts.tsx
+++ src/components/Post/ProfilePosts.tsx
@@ -45,6 +45,11 @@ const ProfilePosts = ({ profileUser, selectedTab }: ProfilePostsProps) => {
   return (
     <div className="h-full flex flex-col">
       <ul className="flex-1">
+        {posts?.length === 0 && (
+          <div className="flex justify-center items-center h-full text-gray-500 font-bold text-xl">
+            <p>작성한 게시글이 없습니다.</p>
+          </div>
+        )}
         {posts?.map(({ title, createdAt, board, _id }) => (
           <li
             className="p-4 border-b border-gray-300 hover:bg-gray-50 transition duration-200"
